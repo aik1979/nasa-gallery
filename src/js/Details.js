30f5f78aeb879aecccbd08eabe6010aee6df91a7
anticitizen4 <20382674+anticitizen4@users.noreply.github.com>
2018-10-28 18:30:41 +0300

    make details placeholder dynamic

--- src/js/Details.js
+++ src/js/Details.js
@@ -1,27 +1,61 @@
 import React, { Component } from "react";
-import { Modal, Image, Header } from "semantic-ui-react";
+import { Modal, Image, Header, Placeholder } from "semantic-ui-react";
 import "../css/Details.scss";
 
-function Details({ current, onClose }) {
-	let open = current !== null;
+const hidden = {
+	display: "none",
+};
+class Details extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { loading: true };
+	}
 
-	let data = (current && current.data) || {};
-	let images = (current && current.images) || [];
+	handleLoad = _ => {
+		this.setState({ loading: false });
+	};
 
-	return (
-		<Modal open={open} onClose={onClose} centered={false}>
-			{/* <Modal.Header>{data.title}</Modal.Header> */}
-			<Modal.Content image>
-				<Image src={images[3]} wrapped className="details__image" />
-				<Modal.Description className="details__description">
-					<Header>{data.title}</Header>
-					<p>{data.center}</p>
-					<p>{data.date_created}</p>
-					<p>{data.description}</p>
-				</Modal.Description>
-			</Modal.Content>
-		</Modal>
-	);
+	handleClose = _ => {
+		this.setState({ loading: true });
+		this.props.onClose();
+	};
+
+	render() {
+		let { current } = this.props;
+		let { loading } = this.state;
+		let open = current !== null;
+
+		let data = (current && current.data) || {};
+		let images = (current && current.images) || [];
+
+		return (
+			<Modal open={open} onClose={this.handleClose} centered={false}>
+				{/* <Modal.Header>{data.title}</Modal.Header> */}
+				<Modal.Content image>
+					<Placeholder
+						className="image details__image"
+						style={(!loading && hidden) || {}}
+					>
+						<Placeholder.Image />
+					</Placeholder>
+					<Image
+						wrapped
+						className="details__image"
+						src={images[3]}
+						style={(loading && hidden) || {}}
+						onLoad={this.handleLoad}
+					/>
+
+					<Modal.Description className="details__description">
+						<Header>{data.title}</Header>
+						<p>{data.center}</p>
+						<p>{data.date_created}</p>
+						<p>{data.description}</p>
+					</Modal.Description>
+				</Modal.Content>
+			</Modal>
+		);
+	}
 }
 
 export default Details;
